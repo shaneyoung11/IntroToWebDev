48952274287a962d5a94543b9ce172acdf1028ab
Shane <shane.young@keyin.com>
2024-03-11 11:04:41 -0230

    commits

--- Class3/maths.js
+++ Class3/maths.js
@@ -10,19 +10,24 @@ val = numb1 / numb2;
 val = numb1 % numb2; // modulus
 
 // // Math Object
-val = Math.PI;
-val = Math.E;
-val = Math.round(3.48);
-val = Math.ceil(3.98);
-val = Math.floor(3.98);
-val = Math.sqrt(49);
-val = Math.abs(-9);
-val = Math.pow(2, 4);
-val = Math.min(3,5,56,43,129,16,87,12,1);
-val = Math.max(3,5,56,43,129,16,87,12,1);
+val = Math.PI; //returns the PI numbers
+val = Math.E; // returns Eulers number
 
+//helpful methods in Math
+val = Math.round(3.48); // does standard rounding
+val = Math.ceil(3.98); // rounds up to the nearest int
+val = Math.floor(3.98); // rounds down to the nearest int
+val = Math.sqrt(49); //square root
+val = Math.abs(-9); // returns the absolute value (no negative values)
+val = Math.pow(2, 4); // returns the powers of number (number, to the power of number)
+val = Math.min(3, 5, 56, 43, 129, 16, 87, 12, 1); //returns the min of the values passed in
+val = Math.max(3, 5, 56, 43, 129, 16, 87, 12, 1); // returns the max of the values passed in
+
+//Random numbers in math
 val = Math.random();
-val = Math.random() * 10;
-val = Math.floor(Math.random() * 10 + 1);
+
+// using random to get a whole integer
+val = Math.random() * 10; // or the max number we want
+val = Math.floor(Math.random() * 10 + 1); // this gives you a number between 1 and 10
 
 console.log(val);
--- Class3/strings.js
+++ Class3/strings.js
@@ -1,57 +1,60 @@
-const firstName = 'Danny';
-const lastName = 'Williams';
+const firstName = "Danny";
+const lastName = "Williams";
 const age = 73;
-const str = 'the fat cat sat on the mat';
-const tags = 'spicy,red hot;mild,needs chaser';
+const str = "the fat cat sat on the mat";
+const tags = "spicy,red hot;mild,needs chaser";
 
 let val;
 val = firstName + lastName;
 
 // Concatenation
-val = firstName + ' ' + lastName;
+val = firstName + " " + lastName;
 
 // Append
-val = 'Roger ';
-val += 'Waters';
+val = "Roger ";
+val += "Waters";
 
-val = 'Hello I am ' + firstName + ', I am now ' + age + ' years old.';
+val = "Hello I am " + firstName + ", I am now " + age + " years old.";
 
 // Escaping
-val = 'Peter\'s fat cat can\'t roll';
+val = "Peter's fat cat can't roll";
 
 // Length
 val = firstName.length;
 
 // concat()
-val = firstName.concat(' ', lastName);
+val = firstName.concat(" ", lastName);
 
 // changing the case
 val = firstName.toLowerCase();
 val = firstName.toUpperCase();
 
-// strings as arrays
+// string indexes
 val = firstName[4];
 
 // indexOf()
-val = lastName.indexOf('l');
-val = lastName.lastIndexOf('l');
+val = lastName.indexOf("l");
+val = lastName.lastIndexOf("l");
 
 // charAt()
 val = firstName.charAt(1);
 val = firstName.charAt(firstName.length - 1);
 
+// substring()
+val = firstName.substring(0, 4);
+
 // slice() Danny
-val = firstName.slice(1,3);
+val = firstName.slice(1, 3);
 val = firstName.slice(-2);
 
-// split()
-val = str.split(' ');
-val = tags.split('i');
+// split() this turns strings into arrays (lists) by splitting the string by what you specify
+val = str.split(" ");
+val = tags.split("i");
 
 // replace()
-val = str.replace('cat', 'bat');
+val = str.replace("cat", "bat");
 
 // includes()
-val = str.includes('mat');
+val = str.includes("mat");
 
 console.log(val);
